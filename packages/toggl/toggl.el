dca63f47a56ca287ab9aa9f45d8a403580cc766f
Chris Done <chrisdone@gmail.com>
2017-02-06 15:50:04 +0000

    Good stuff

--- packages/toggl/toggl.el
+++ packages/toggl/toggl.el
@@ -62,7 +62,38 @@
         (search-forward-regexp "\r?\n\r?\n" nil nil 1)
         (json-read)))))
 
-(defun toggl-submit (description start-date-time duration)
+(defun toggl-project-id-by-name (name)
+  (cdr
+   (assoc
+    'id
+    (car (cl-remove-if-not
+          (lambda (p)
+            (string= (cdr (assoc 'name p)) name))
+          (mapcar 'identity toggl-client-projects))))))
+
+(defun toggl-entries (start-date end-date)
+  "Synchronously return all entries within START-DATE and END-DATE."
+  (unless toggl-api-key
+    (error "Set the value of `toggle-api-key' to your key."))
+  (let ((url-request-method "GET")
+        (url-request-extra-headers
+         `(("Content-Type" .
+            "Content-Type: application/json")
+           ("Authorization" .
+            ,(concat "Basic "
+                     (base64-encode-string
+                      (concat toggl-api-key ":api_token")))))))
+    (let ((buffer (url-retrieve-synchronously
+                   (format "https://www.toggl.com/api/v8/time_entries?start_date=%s&end_date=%s"
+                           (url-hexify-string (toggl-format-time start-date))
+                           (url-hexify-string (toggl-format-time end-date)))
+                   t)))
+      (with-current-buffer buffer
+        (goto-char (point-min))
+        (search-forward-regexp "\r?\n\r?\n" nil nil 1)
+        (json-read)))))
+
+(defun toggl-submit (project-id description start-date-time duration)
   "Asynchronously submit a tracking item to toggl.
 
 DESCRIPTION is the name of the item, START-DATE-TIME should be a
@@ -85,13 +116,18 @@ standard Emacs date-time, and DURATION is a number of seconds."
             :description description
             :created_with "Emacs"
             :billable t
-            :start (replace-regexp-in-string "\\(.+?\\)\\+\\([0-9][0-9]\\)\\([0-9][0-9]\\)" "\\1+\\2:\\3" (format-time-string "%Y-%m-%dT%H:%M:%S%z" start-date-time))
+            :pid project-id
+            :start (toggl-format-time start-date-time)
             :duration duration)))))
-    (url-retrieve
-     "https://www.toggl.com/api/v8/time_entries"
-     (lambda (&rest args)
-       (when (plist-get args :error)
-         (error "Error submitting to toggl: %S" (plist-get args :error))))
-     t)))
+    (let ((buffer
+           (url-retrieve-synchronously
+            "https://www.toggl.com/api/v8/time_entries"
+            t)))
+      (with-current-buffer buffer
+        (goto-char (point-min))
+        (looking-at "HTTP/1.1 200 OK")))))
+
+(defun toggl-format-time (start-date-time)
+  (replace-regexp-in-string "\\(.+?\\)\\+\\([0-9][0-9]\\)\\([0-9][0-9]\\)" "\\1+\\2:\\3" (format-time-string "%Y-%m-%dT%H:%M:%S%z" start-date-time)))
 
 (provide 'toggl)
